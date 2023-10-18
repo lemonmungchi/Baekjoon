a7eb6d298173b90345fd5b598b25e11d6960d594
lemonmungchi <khc112353@gmail.com>
2023-10-18 22:04:15 +0900

    2문제
    2문제완료

---
+++ part4/b_p7.py
@@ -0,0 +1,5 @@
+import sys
+
+for i in range(100):
+    s=str(sys.stdin.readline().rstrip())
+    print(s,end="\n")
---
+++ part4/b_p8.py
@@ -0,0 +1,28 @@
+import sys
+
+li=[]
+li=list(map(int,sys.stdin.readline().split()))
+if(li[0]<1):
+    print(1-li[0],end=' ')
+else:
+    print(1 - li[0], end=' ')
+if(li[1]<1):
+    print(1-li[1],end=' ')
+else:
+    print(1 - li[1], end=' ')
+if(li[2]<2):
+    print(2-li[2],end=' ')
+else:
+    print(2 - li[2], end=' ')
+if(li[3]<2):
+    print(2-li[3],end=' ')
+else:
+    print(2 - li[3], end=' ')
+if(li[4]<2):
+    print(2-li[4],end=' ')
+else:
+    print(2 - li[4], end=' ')
+if(li[5]<8):
+    print(8-li[5],end=' ')
+else:
+    print(8 - li[5], end=' ')
