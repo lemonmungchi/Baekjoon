cee462d4aaea96f68ff82e305a8a4734b31bcd31
lemonmungchi <khc112353@gmail.com>
2023-11-06 17:54:17 +0900

    3문제 문자열
    3문제 문자열

---
+++ ConsoleApplication1/b_p5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string s;
+	int s_n;
+
+	cin >> s;
+	cin >> s_n;
+
+	cout << s.at(s_n-1);
+
+}
---
+++ ConsoleApplication1/b_p7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		string s;
+
+		cin >> s;
+		cout << s.at(0) << s.at(s.length()-1)<<"\n";
+	}
+
+}
