527a907bb0aae7c761a16b4b8f2e441ae385c29f
Hyeon Chan <116485176+lemonmungchi@users.noreply.github.com>
2024-09-09 16:07:01 +0900

    9.9 오후

---
+++ ConsoleApplication1/b19.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if (a + b + c != 180)
+        cout << "Error" << endl;
+    else
+    {
+        if (a == 60 && b == 60 && c == 60)
+            cout << "Equilateral" << endl;
+        else if (a == b || b == c || a == c)
+            cout << "Isosceles" << endl;
+        else
+            cout << "Scalene" << endl;
+    }
+
+    return 0;
+}
