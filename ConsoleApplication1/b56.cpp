77e67f364cec66b02394e47cb3051d6202bf5178
Hyeon Chan <116485176+lemonmungchi@users.noreply.github.com>
2024-10-16 14:45:07 +0900

    10.16

---
+++ ConsoleApplication1/b56.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[51][51][51];
+
+int w(int a, int b, int c) 
+{
+	if (a <= 0 || b <= 0 || c <= 0)
+		return 1;
+
+	if (a > 20 || b > 20 || c > 20) 
+		return w(20, 20, 20);
+
+	// 이미 저장된 값이 있다면 재활용 : DP
+	if (dp[a][b][c] != 0) {
+		return dp[a][b][c];
+	}
+
+	if (a < b && b < c)
+	{
+		dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+	}
+	else
+	{
+		dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+	}
+
+	return dp[a][b][c];
+}
+
+int main()
+{
+	int a, b, c;
+
+
+
+	while (true)
+	{
+		cin >> a >> b >> c;
+		if (a == -1 && b == -1 && c == -1) break;
+
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << endl;
+	}
+
+	return 0;
+}
